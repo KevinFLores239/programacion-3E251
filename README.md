69598ba1c5ca06b73d513d482156e0610be0ff7e
KevinFLores239 <a24110232@live.ceti.mx>
2025-04-03 02:01:43 +0000

    practica 3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # programacion-3E251
 
 c++ main.cpp -o programa -lftxui-component -lftxui-dom -lftxui-screen -std=c++17
+
+
+//si ponemos $< significa main.cpp y si pongo $@ hace referencia a programa 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <iostream>
+#include <string>
+#include <thread>
 using namespace std;
 using namespace ftxui;
 int main(int argc, char const *argv[])
@@ -11,12 +13,21 @@ auto pantalla = Screen::Create (
     Dimension::Full(),
     Dimension::Full()
 );
+int fotograma = 0;
+string resetPosition;
+while(true) {     
 auto documento = vbox(
-    spinner(21,1)
-
+    spinner(21, fotograma)
 );
 Render(pantalla, documento);
+cout <<resetPosition;
 pantalla.Print();
+resetPosition = pantalla.ResetPosition();
+
+fotograma ++;
+std::this_thread::sleep_for(0.04s);
+}
+
     //ftxui::Screen pantalla(10,10);
     
     Serie serienavidena;
