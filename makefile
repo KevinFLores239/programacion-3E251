1a282de1420622fe12e4cc36db095c65fd619e1c
KevinFLores239 <a24110232@live.ceti.mx>
2025-04-04 03:17:34 +0000

    practica 4

--- makefile
+++ makefile
@@ -3,8 +3,14 @@ LIB := -lftxui-component -lftxui-dom -lftxui-screen
 CXX := -std=c++17
 SRC := src
 INCLUDE := include
+
 BIN := bin
 EXE := $(BIN)/$(PROYECTO)
+
+DATOS = datos
+TYPE := $(BIN)/$(DATOS)
+
+
 
 $(EXE) : $(SRC)/main.cpp
 	c++ $< -o $@ $(LIB) $(CXX) -I$(INCLUDE)
@@ -16,3 +22,12 @@ run : $(EXE)
 
 clean : 
 	rm $(BIN)/*
+
+
+$(TYPE) : $(SRC)/memoria.cpp
+	c++ $< -o $@ $(LIB) $(CXX)
+
+ejecutar : $(TYPE)
+	./$<
+
+
---
+++ src/memoria.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"int: "<<sizeof(int)<<" bytes "<<endl;
+    cout<<"char: "<<sizeof(char)<<" bytes "<<endl;
+    cout<<"float: "<<sizeof(float)<<" bytes "<<endl;
+    cout<<"double: "<<sizeof(double)<<" bytes "<<endl;
+    cout<<"bool: "<<sizeof(bool)<<" bytes "<<endl;
+
+    cout<<"Operador Direccion"<<endl;
+int a = 74, b = 185;
+    
+    cout<<"Direccion de a: "<<&a<<endl;
+    cout<<"Direccion de b: "<<&b<<endl;
+
+int * direccion = &a;
+cout<<"Direccion a: "<<direccion<<endl;
+cout<<*direccion<<endl;//imprime el valor a donde a punta 
+cout<<&direccion<<endl;//imprime direccion del puntero 
+cout<<sizeof(direccion)<<endl;//el peso 
+}
